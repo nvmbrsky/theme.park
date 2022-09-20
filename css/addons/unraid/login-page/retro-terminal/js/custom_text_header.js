edd4354e42c450c1bb52918d7a9847bb210f54d6
nvmbrsky <113980654+nvmbrsky@users.noreply.github.com>
2022-09-20 18:31:30 +0800

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -6,11 +6,23 @@
 // YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>                     __                             ___       __  
-   ____  ____  _____/ /__________  ____ ___  ____  / (_)___  / /__
-  / __ \\/ __ \\/ ___/ __/ ___/ __ \\/ __ '__ \\/ __ \\/ / / __ \\/ //_/
- / / / / /_/ (__  ) /_/ /  / /_/ / / / / / / /_/ / / / / / / ,&lt;   
-/_/ /_/\\____/____/\\__/_/   \\____/_/ /_/ /_/\\____/_/_/_/ /_/_/|_|  
-                                                                 </pre> </div>
+<div class="custom-text-header"> <pre>
+ ,ggg, ,ggggggg,                                                                                                ,gg,                         
+dP""Y8,8P"""""Y8b                                                          ,dPYb,                              i8""8i   ,dPYb,               
+Yb, `8dP'     `88                                                          IP'`Yb                              `8,,8'   IP'`Yb               
+ `"  88'       88                                                          I8  8I                               `88'    I8  8I               
+     88        88                                                          I8  8'                               dP"8,   I8  8bgg,            
+     88        88    ,ggggg,       ggg    gg    ,ggg,    ,ggg,,ggg,,ggg,   I8 dP        ,ggg,    ,gggggg,      dP' `8a  I8 dP" "8  gg     gg 
+     88        88   dP"  "Y8ggg   d8"Yb   88bg i8" "8i  ,8" "8P" "8P" "8,  I8dP   88gg i8" "8i   dP""""8I     dP'   `Yb I8d8bggP"  I8     8I 
+     88        88  i8'    ,8I    dP  I8   8I   I8, ,8I  I8   8I   8I   8I  I8P    8I   I8, ,8I  ,8'    8I _ ,dP'     I8 I8P' "Yb,  I8,   ,8I 
+     88        Y8,,d8,   ,d8'  ,dP   I8, ,8I   `YbadP' ,dP   8I   8I   Yb,,d8b,  ,8I   `YbadP' ,dP     Y8,"888,,____,dP,d8    `Yb,,d8b, ,d8I 
+     88        `Y8P"Y8888P"    8"     "Y8P"   888P"Y8888P'   8I   8I   `Y88P'"Y88P"'  888P"Y8888P      `Y8a8P"Y88888P" 88P      Y8P""Y88P"888
+                                                                                                                                        ,d8I'
+                                                                                                                                      ,dP'8I 
+                                                                                                                                     ,8"  8I 
+                                                                                                                                     I8   8I 
+                                                                                                                                     `8, ,8I 
+                                                                                                                                      `Y8P"  
+</pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
